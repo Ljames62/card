dcfc19415b8e32833587d26bda0f1a3df8e93ef9
Ljames62 <63357541+Ljames62@users.noreply.github.com>
2026-01-17 02:18:01 -0500

    Add files via upload

---
+++ ann27.config.js
@@ -0,0 +1,12 @@
+window.CARD_CONFIG = {
+  orientation: 'landscape', // landscape | portrait
+
+  images: {
+    front: "images/gin-ann27-ginnie-and-john-fais-do-do.jpg",
+    insideTop: "images/gin-ann27-ginnie-and-john-olympic.jpg",
+    insideBottom: "images/gin-ann27-ginnie-and-john-lumineers.jpg",
+    back: "images/gin-ann27-penn-canyon.jpg"
+  },
+
+  audio: "audio/gin-ann27.mp3"
+};
---
+++ index.config.js
@@ -0,0 +1,12 @@
+window.CARD_CONFIG = {
+  orientation: 'landscape', // landscape | portrait
+
+  images: {
+    front: 'images/idx-antelope.jpg',
+    insideTop: 'images/idx-zion.jpg',
+    insideBottom: 'images/idx-bryce.jpg',
+    back: 'images/idx-penn.jpg'
+  },
+
+  audio: 'audio/voice-of-nature-296319.mp3'
+};
