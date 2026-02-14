28aa5f0fd913a41bd84e7f0539a525bebcaf07ef
Ljames62 <63357541+Ljames62@users.noreply.github.com>
2026-02-14 02:41:41 -0500

    Add files via upload

---
+++ js/val26.config.js
@@ -0,0 +1,12 @@
+window.CARD_CONFIG = {
+  orientation: 'landscape', // landscape | portrait
+
+  images: {
+    front: "images/gin-val26-heart.jpg",
+    insideTop: "images/gin-val26-flower01.jpg",
+    insideBottom: "images/gin-val26-flower02.jpg",
+    back: "images/gin-val26-flower03.jpg"
+  },
+
+  audio: "audio/gin-val26.mp3"
+};
