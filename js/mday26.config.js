fcc0aab7ede219c5b9735c28fa04cbb0a63584bc
Ljames62 <63357541+Ljames62@users.noreply.github.com>
2026-05-09 14:11:07 -0500

    Add files via upload

---
+++ js/mday26.config.js
@@ -0,0 +1,12 @@
+window.CARD_CONFIG = {
+  orientation: 'landscape', // landscape | portrait
+
+  images: [
+    "images/gin-mday26-00.jpg",
+    "images/gin-mday26-01.jpg",
+    "images/gin-mday26-02.jpg",
+    "images/gin-mday26-03.jpg"
+  ],
+
+  audio: "audio/gin-mday26.mp3"
+};
